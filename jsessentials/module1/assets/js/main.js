6c3cbba4f04fec01bee29b17c80827f7683a85fd
hardingrichard <richard.jharding@hotmail.co.uk>
2024-07-22 16:16:00 -0400

    Add function numberValidate for validation in index.html

--- jsessentials/module1/assets/js/main.js
+++ jsessentials/module1/assets/js/main.js
@@ -13,3 +13,20 @@ function styleChange() {
     text.style.fontSize = "25px";
     text.style.color = "red";
 }
+
+function numberValidate() {
+    let x, text;
+
+    // Get input field value from id="numb"
+    x = document.getElementById("numb").value;
+
+    // If x is Not a Number or less than one or greater than 10
+    if (isNaN(x) || x < 1 || x > 10) {
+        text = "Input not valid";
+    } else {
+        text = "Input OK";
+    }
+
+    // Display result
+    document.getElementById("demo3").innerHTML = text;
+}
