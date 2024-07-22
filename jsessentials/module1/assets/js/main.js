95376cfb489748c57d0fd796774863acdf5cdab6
hardingrichard <richard.jharding@hotmail.co.uk>
2024-07-22 12:12:57 -0400

    Add styleChange function

--- jsessentials/module1/assets/js/main.js
+++ jsessentials/module1/assets/js/main.js
@@ -7,3 +7,9 @@ function changeImage() {
             image.src = '../module1/assets/media/images/bones.png';
     }
 }
+
+function styleChange() {
+    let text = document.getElementById("demo2")
+    text.style.fontSize = "25px";
+    text.style.color = "red";
+}
