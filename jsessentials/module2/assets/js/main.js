0d9a577b4fab8241a5bc4a9e19de257c421fe750
hardingrichard <richard.jharding@hotmail.co.uk>
2024-07-23 16:10:18 -0400

    Add console.logs to test output

---
+++ jsessentials/module2/assets/js/main.js
@@ -0,0 +1,16 @@
+let car = "Mercedes";
+let van = "Ford";
+let myNumber = 55;
+let myString = "55"
+
+let firstName = "Rich";
+let lastName = "Harding";
+let age = 35;
+let myMessage = firstName + " " + lastName + " is " + age + " years old.";
+
+console.log(car);
+console.log(van);
+console.log(myNumber);
+console.log(myString);
+console.log(myNumber + myString);
+console.log(myMessage);
