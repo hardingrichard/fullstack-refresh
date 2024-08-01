a825815e758b79b4d1425de92a030799f01ff1b5
hardingrichard <richard.jharding@hotmail.co.uk>
2024-08-01 13:17:47 -0400

    Add hide and show function for buttons to manipulate the p elements

--- jsessentials/module4/assets/js/main.js
+++ jsessentials/module4/assets/js/main.js
@@ -4,10 +4,10 @@
 // jQuery can be used to select elements by id, class, type, attribute, etc.
 // jQuery can be used to perform actions on elements like hiding, showing, adding classes, etc.
 
-$(this).hide(); // hides the current element
-$("p").hide(); // hides all <p> elements
-$(".test").hide(); // hides all elements with class="test"
-$("#test").hide(); // hides the element with id="test"
+// $(this).hide(); - hides the current element
+// $("p").hide(); - hides all <p> elements
+// $(".test").hide(); - hides all elements with class="test"
+// $("#test").hide(); - hides the element with id="test"
 
 
 // Comparison javaScript vs jQuery
@@ -25,4 +25,29 @@ function myFunction () {
 
 $(document).ready(myFunction);
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+function myTitle() {
+    $("#title").html("jQuery test"); // changes the text of the h1 element
+}
+
+$(document).ready(myTitle); // calls the function when the document is ready
+
+function installSection() {
+    $("h2").attr("style", "color:darkblue").html("Another jQuery test"); // changes the text and color of the h2 element
+}
+
+$(document).ready(installSection);
+
+$(document).ready(function () {
+    $("button").click(function() {
+        $("p").hide();
+    })
+})
+
+$(document).ready(function () {
+    $("button").eq(1).click(function() { 
+        $("p").show();
+        // eq() method selects the element at the specified index, this case 1 would select the second button.
+    })
+})
 
