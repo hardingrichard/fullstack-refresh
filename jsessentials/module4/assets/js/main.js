1e8b35077e3b2f574995da1eb8e84c9296b5fda0
hardingrichard <richard.jharding@hotmail.co.uk>
2024-08-01 13:31:06 -0400

    Fix jQuery functions and update styles

--- jsessentials/module4/assets/js/main.js
+++ jsessentials/module4/assets/js/main.js
@@ -16,38 +16,44 @@
 function myFunction () {
     let obj = document.getElementById("test");
     obj.innerHTML = "javaScript example";
-}
+};
 
 //jQuery
 function myFunction () {
     $("#test").html("jQuery example");
-}
+};
 
 $(document).ready(myFunction);
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 function myTitle() {
     $("#title").html("jQuery test"); // changes the text of the h1 element
-}
+};
 
 $(document).ready(myTitle); // calls the function when the document is ready
 
 function installSection() {
     $("h2").attr("style", "color:darkblue").html("Another jQuery test"); // changes the text and color of the h2 element
-}
+};
 
 $(document).ready(installSection);
 
 $(document).ready(function () {
     $("button").click(function() {
         $("p").hide();
-    })
-})
+    });
+});
 
 $(document).ready(function () {
     $("button").eq(1).click(function() { 
         $("p").show();
         // eq() method selects the element at the specified index, this case 1 would select the second button.
-    })
-})
+    });
+});
 
+$("button").eq(2).click(function() {
+    $("h2").eq(0).attr("style", "color:#333").html("Installing And Using jQuery");
+    $("h2").eq(1).attr("style", "color:#333").html("Modifying Web Pages Using jQuery")
+    $("p").show();
+    $("#title").html("Working With Libraries: jQuery");
+});
