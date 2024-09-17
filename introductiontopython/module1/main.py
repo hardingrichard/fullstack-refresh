8749de03adf56cc4b30162cecb295f40857d15a6
hardingrichard <richard.jharding@hotmail.co.uk>
2024-09-17 20:42:26 +0100

    Add type checking and conversion code

--- introductiontopython/module1/main.py
+++ introductiontopython/module1/main.py
@@ -7,3 +7,15 @@ z = True
 print(type(x))
 print(type(y))
 print(type(z))
+
+print(isinstance(x, float))
+print(isinstance(y, float))
+print(isinstance(z, float))
+print(isinstance(y, int))
+
+number = 32
+another_number = 10
+
+print(float(number))
+print(int(x))
+print(float(another_number))
