5d7de322ef88eae1a2d54ff0dfcc5d80ee332814
hardingrichard <richard.jharding@hotmail.co.uk>
2024-09-17 20:56:06 +0100

    Add variable assignments and print statements

--- introductiontopython/module1/main.py
+++ introductiontopython/module1/main.py
@@ -19,3 +19,18 @@ another_number = 10
 print(float(number))
 print(int(x))
 print(float(another_number))
+
+first_string = "hello"
+second_string = first_string
+
+print(first_string)
+print(second_string)
+
+first_string = "world"
+
+print(first_string)
+print(second_string)
+
+second_string = first_string
+
+print(second_string)
